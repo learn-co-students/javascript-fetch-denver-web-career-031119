acaa5d844ec0720db2782f47c35b54f2ff8f89c3
DangerN <cdneal091@gmail.com>
2019-04-26 14:05:19 -0600

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+
+const token = '1ee9d2b9fcbc4cc111e8c4d2d585c81e3e770659';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
